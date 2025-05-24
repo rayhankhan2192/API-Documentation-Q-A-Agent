766585577e8b84cb409932ce8042864491fba4fd
Rayhan Khan <rayhankhan1226@gmail.com>
2025-05-25 02:26:00 +0600

    Initial changes

--- Scripts/retrieve.py
+++ Scripts/retrieve.py
@@ -21,23 +21,3 @@ def get_top_k_chunks(
         "metadatas": results.get("metadatas", [[]])[0],
         "distances": results.get("distances", [[]])[0],
     }
-
-
-# # === retrieve.py ===
-# from sentence_transformers import SentenceTransformer
-# from chromadb import PersistentClient
-
-# def get_top_k_chunks(question: str, k: int = 5, persist_dir: str = "embeddings/chromadb/", collection_name: str = "api_docs"):
-#     client = PersistentClient(path=persist_dir)
-#     collection = client.get_collection(collection_name)
-
-#     model = SentenceTransformer('all-MiniLM-L6-v2')
-#     query_embedding = model.encode([question])[0]
-
-#     results = collection.query(query_embeddings=[query_embedding], n_results=k)
-#     return {
-#         "documents": results.get("documents", [[]])[0],
-#         "metadatas": results.get("metadatas", [[]])[0],
-#         "distances": results.get("distances", [[]])[0],
-#     }
-
